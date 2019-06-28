a10748877924aa3b42a19811ac24e51a7f0696f3
Brooke Jones <bjones782@gmail.com>
2019-06-28 14:10:38 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Student.create([{ first_name: "Brooke", last_name: "Jones" }, { first_name: "Lauren", last_name: "Jones" }])
